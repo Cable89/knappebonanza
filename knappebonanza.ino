77f2a32e084e5779daf608e7a94c71bb855997d6
Øystein Smith <oystein.smith89@gmail.com>
2019-12-22 19:55:59 +0100

    basic functionality working

--- knappebonanza.ino
+++ knappebonanza.ino
@@ -17,27 +17,44 @@ CRGB leds[NUM_LEDS];
 #define FRAMES_PER_SECOND  120
 
 
-#define BTN_ARCADE_RED    
-#define BTN_ARCADE_ORANGE
-#define BTN_ARCADE_YELLOW
-#define BTN_ARCADE_PINK
-#define BTN_ARCADE_BLACK
-#define BTN_ARCADE_GREEN
-#define BTN_BLUE
-#define BTN_RED
-#define BTN_BLACK
-#define BTN_FLIP1_UP
-#define BTN_FLIP1_DOWN
-#define BTN_FLIP2_UP
-#define BTN_FLIP2_DOWN
+#define BTN_ARCADE_RED    1  // 0
+#define BTN_ARCADE_ORANGE 9  // 1
+#define BTN_ARCADE_YELLOW 16 // 2
+#define BTN_ARCADE_PINK   15 // 3
+#define BTN_ARCADE_BLACK  10 // 4
+#define BTN_ARCADE_GREEN  14 // 5
+#define BTN_BLUE          0  // 6
+#define BTN_RED           3  // 7
+#define BTN_BLACK         4  // 8
+#define BTN_FLIP1_DOWN    5  // 9
+#define BTN_FLIP1_UP      6  // 10
+#define BTN_FLIP2_DOWN    8  // 11
+#define BTN_FLIP2_UP      7  // 12
 
-#define POT_TRIM
-#define POT_SMALL
-#define POT_MEDIUM
-#define POT_LARGE
+int buttons[13] = {BTN_ARCADE_RED, BTN_ARCADE_ORANGE, BTN_ARCADE_YELLOW, BTN_ARCADE_PINK, BTN_ARCADE_BLACK, BTN_ARCADE_GREEN, BTN_BLUE, BTN_RED, BTN_BLACK, BTN_FLIP1_UP, BTN_FLIP1_DOWN, BTN_FLIP2_UP, BTN_FLIP2_DOWN};
+int buttonvals[13];
+
+#define POT_TRIM          3
+#define POT_SMALL         2
+#define POT_MEDIUM        1
+#define POT_LARGE         0
 
 
 void setup() {
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(9, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  
   // put your setup code here, to run once:
   //delay(3000); // 3 second delay for recovery
   
@@ -51,26 +68,51 @@ void setup() {
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+SimplePatternList gPatterns = { buttonDebug, rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop() {
   // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+  //gPatterns[gCurrentPatternNumber]();
+  //gPatterns[0](); // buttondebug
 
   // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
+  FastLED.show();
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND); 
 
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  //EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+  //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  
+  for(int i=0; i<9; i++){
+    //buttonsvals[i] = digitalRead(buttons[i]);
+    if((buttons[i] != BTN_RED)){
+      if(digitalRead(buttons[i]) == LOW){
+       gPatterns[i%6]();
+      }
+    }
+  }
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void buttonDebug(){
+  for(int i=0; i<14; i++){
+    //buttonsvals[i] = digitalRead(buttons[i]);
+    if((buttons[i] != BTN_RED)){
+      if(digitalRead(buttons[i]) == LOW){
+       leds[i] = CHSV(25,200,255);
+      } else {
+       leds[i] = CHSV(25,0,0);
+      }
+    }
+  }
+}
 
 void nextPattern()
 {
